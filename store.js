6280649ecd9d102cd778231e0f00d94ee8699c15
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-02-08 12:25:37 -0600

    the store of variables

--- store.js
+++ store.js
@@ -1,3 +1,8 @@
 var $itemOne = document.getElementById("itemOne");
+var likeList = ["poscas", "spreadsheets", "vintage maps", "chicken"];
 
-var likelist = ["poscas", "spreadsheets", "vintage maps", "chicken"];
+function randomItem() {
+  var randomIndex = Math.floor(Math.random() * likeList.length);
+  var randomLike = likeList[randomIndex];
+  $itemOne.innerHTML = randomLike;
+}
