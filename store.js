bfac96dd40456503a5608e8faff395e9cb23f692
loreleim <lorelei.miyamura@gmail.com>
2020-02-09 13:41:44 -0600

    filling the likeList with a little more items

--- store.js
+++ store.js
@@ -1,7 +1,9 @@
 var $itemOne = document.getElementById("itemOne");
 var $itemTwo = document.getElementById("itemTwo");
 var $itemThree = document.getElementById("itemThree");
-var likeList = ["poscas", "spreadsheets", "vintage maps"];
+var likeList = ["posca markers", "spreadsheets", "topographic maps", "azuki beans", "pogs", "pocket notebooks", "menpachi"];
+var randomIndex = Math.floor(Math.random() * likeList.length);
+var randomLike = likeList[randomIndex];
 
 $itemOne.innerHTML = likeList[0] + ",";
 $itemTwo.innerHTML = likeList[1] + ",";
