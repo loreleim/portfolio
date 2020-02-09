07d518d12a6a0d7fb6b624969e5d646296663cdc
loreleim <lorelei.miyamura@gmail.com>
2020-02-09 14:18:01 -0600

    more styling for the about page

--- store.js
+++ store.js
@@ -1,16 +1,22 @@
-var $itemOne = document.getElementById("itemOne");
-var $itemTwo = document.getElementById("itemTwo");
-var $itemThree = document.getElementById("itemThree");
-var likeList = ["posca markers", "spreadsheets", "topographic maps", "azuki beans", "pogs", "pocket notebooks", "menpachi"];
-var randomIndex = Math.floor(Math.random() * likeList.length);
-var randomLike = likeList[randomIndex];
+var itemOne = document.getElementById("itemOne");
+var itemTwo = document.getElementById("itemTwo");
+var itemThree = document.getElementById("itemThree");
+var likeList = [
+  "posca markers",
+  "spreadsheets",
+  "topographic maps",
+  "azuki beans",
+  "pogs",
+  "pocket notebooks",
+  "menpachi"
+];
 
-$itemOne.innerHTML = likeList[0] + ",";
-$itemTwo.innerHTML = likeList[1] + ",";
-$itemThree.innerHTML = "& " + likeList[2] + "!";
+itemOne.innerHTML = likeList[0] + ",";
+itemTwo.innerHTML = likeList[1] + ",";
+itemThree.innerHTML = "& " + likeList[2] + "!";
 
-function randomItem() {
+/*function randomItem() {
   var randomIndex = Math.floor(Math.random() * likeList.length);
   var randomLike = likeList[randomIndex];
   $itemOne.innerHTML = randomLike;
-}
+}*/
